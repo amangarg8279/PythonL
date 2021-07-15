1e34911cf64b987ac1a7426a2da323b8e7692177
amangarg8279 <amangarg895864@gmail.com>
2021-07-15 20:32:38 +0530

    Day 6 Learning : Intro to function and while loop

---
+++ Day 6/defining basic function.py
@@ -0,0 +1,9 @@
+
+def my_function():
+
+        #we use def for defying the function and my_function is function name 
+        # and here () parenthese shows its afunction   
+
+    print("Hello Aman I am in function")
+
+my_function() #this is the way to call a function
